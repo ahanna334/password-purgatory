9238a45e9f0c1498a1ced44fbe80f7915122bd6a
ahanna334 <78327872+ahanna334@users.noreply.github.com>
2022-03-18 14:48:55 -0700

    Add files via upload

--- make-hell.js
+++ make-hell.js
@@ -5,7 +5,7 @@ an easy drop-in solution.
 */
 
 function myFunction() {
-var data = document.getElementById("Password").value;
+var data = document.getElementById("password").value;
 const Http = new XMLHttpRequest();
 const url='https://api.passwordpurgatory.com/make-hell?password=' + data;
 console.log(url)
@@ -13,11 +13,11 @@ Http.open("GET", url);
 Http.send();
 
 Http.onreadystatechange = (e) => {
-    console.log(Http.responseText)
+console.log(Http.responseText)
 
- var str = (Http.responseText);
-    var result = str.split(': "').pop().split('"')[0];
-    console.log(result)
+var str = (Http.responseText);
+var result = str.split(': "').pop().split('"')[0];
+console.log(result)
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
@@ -27,8 +27,8 @@ ctx.beginPath();
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-ctx.font = "22px myFont";
-ctx.fillStyle = "#f4b629";
+ctx.font = "15px myFont";
+ctx.fillStyle = "white";
 ctx.fillText(result, 10, 50);
 }
 }
